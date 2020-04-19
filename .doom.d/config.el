e93bc3bc9d61ce48f70b3ff932d1bf1933f1fd15
Dũng (Trần Đình) <dungtd@trobz.com>
2020-04-19 10:29:14 +0700

    Update dotfiles

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Dzung Tran"
+      user-mail-address "d2tran7@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
