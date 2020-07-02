081f9dba072ef3d9bd970e08e343fb77c48e3667
Dung.Tran-Dinh <dungtd@trobz.com>
2020-07-02 17:42:39 +0700

    [ADD] Update dotfiles

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 
 ## Env variables
 export E=/Volumes/ex01
-export EHOME=$E/Library/Containers
 
 ## Symbolic links
 
 # Create a symlink to External Drive to preserve internal drive.
-ln -sf $EHOME/com.docker.docker $HOME/Library/Containers
+ln -sf $E/Library/Containers/com.docker.docker $HOME/Library/Containers
+
+# zsh
+
+[[ ! -f ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]] || \
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Projects
 
